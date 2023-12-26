34a5d5ab991bea691167b959ebc7bd9946c2ee27
Diego Paulo <58493827+DiegoPauloTK147@users.noreply.github.com>
2023-12-26 11:16:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,167 @@
-'use strict';
-
-const limparFormulario = (endereco) =>{
-    document.getElementById('endereco').value = '';
-    document.getElementById('bairro').value = '';
-    document.getElementById('cidade').value = '';
-    document.getElementById('estado').value = '';
-    document.getElementById('CNPJ').value = '';
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap');
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
 }
 
+:root {
+    --bg-color: #000;
+    --primary-color: #FFFFFF;
+}
 
-const preencherFormulario = (endereco) =>{
-    document.getElementById('endereco').value = endereco.logradouro;
-    document.getElementById('bairro').value = endereco.bairro;
-    document.getElementById('cidade').value = endereco.localidade;
-    document.getElementById('estado').value = endereco.uf;
+body {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    min-height: 100vh;
+    background-color: var(--bg-color) ;
+}
+
+.logo__ajust {
+    width: 50%;
+    text-align: center;
+    align-items: center;
+    padding: 20px;
+}
+
+.container {
+    flex-grow: 3;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 80%;
+    gap: 2.5rem;
+}
+
+.paragrafo {
+    color: #FFFFFF;
+    font-size: 1.4rem;
+}
+
+.title {
+    font-size: 40px;
+    text-align: center;
+    user-select: none;
+    color: var(--primary-color);
+}
+
+.row {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+    gap: 2.5rem;
+}
+.inputbox {
+    position: relative;
+    display: flex;
+    flex-direction: column-reverse;
+    height: 2.5rem;
+}
+
+.inputbox label {
+    position: relative;
+    top:0;
+    left: 10px;
+    font-size: 20px;
+    color: var(--primary-color);
+    transition: .5s;
+    cursor: text;
+}
+
+.inputbox input {
+    position: absolute;
+    background-color: var(--primary-color);
+    width: 100%;
+    height: 2px;
+    bottom: 0;
+    box-shadow: none;
+    border: none;
+    outline: none;
+    border-radius: 10px;
+    transition: .5s;
+    font-size: 20px;
+    font-weight: bold;
+    padding: 0 10px;
+}
+
+.inputbox input:focus,
+.inputbox input:valid {
+    height: 100%;
+}
+
+.inputbox input:focus + label,
+.inputbox input:valid + label {
+    top: -40px;
+    left: 0;
+}
+
+.container button {
+    justify-self: center;
+    width: 200px;
+    height: 50px;
+    border:none;
+    outline: none;
+    cursor: pointer;
+    background-color: var(--primary-color);
+    font-size: 20px;
+    font-weight: bold;
+    border-radius: 2px;
+}
+
+footer {
+    color: var(--primary-color);
+    padding: 20px;
+    align-items: center;
+    text-align: center;
+}
+
+.final{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    color: aliceblue;
+    align-items: center;
+    text-align: center;
+    padding: 0px;
+}
+
+.logo__rem{
+    width: 300px;
+}
+
+.final__botão {
+    background-color: green;
+    padding: 10px;
+    border-radius: 10px;
+
 
 }
 
-
-const eNumero = (numero) => /^[0-9]+$/.test(numero);
-
-const cepValido = (cep) => cep.length == 8 && eNumero(cep); 
-
-const pesquisarCep = async() => {
-    limparFormulario();
-    
-    const cep = document.getElementById('cep').value;
-    const url = `https://viacep.com.br/ws/${cep}/json/`;
-    if (cepValido(cep)){
-        const dados = await fetch(url);
-        const endereco = await dados.json();
-        if (endereco.hasOwnProperty('erro')){
-            document.getElementById('endereco').value = 'CEP não encontrado!';
-        }else {
-            preencherFormulario(endereco);
+@media screen and (max-width: 1080px) {
+    body {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        min-height: 100vh;
+        background-color: var(--bg-color);
         }
-    }else{
-        document.getElementById('endereco').value = 'CEP incorreto!';
-    }
-     
+    .container {
+            flex-grow: 3;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            width: 95%;
+            gap: 2.5rem;
+        }
+    .logo__ajust {
+            width: 80%;
+            text-align: center;
+            align-items: center;
+            padding: 20px;
+        }
 }
-
-document.getElementById('cep')
-        .addEventListener('focusout',pesquisarCep);
